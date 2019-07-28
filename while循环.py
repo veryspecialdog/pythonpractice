0bc9534acafcda06ebadbe5df06e86f8caa858ad
veryspecialdog <13141360658@163.com>
2019-07-29 00:08:25 +0800

    practice1

---
+++ while循环.py
@@ -0,0 +1,20 @@
+
+x = input("input x:")
+y = input("input y:")
+c = input("input c:")
+
+if c=="/":
+    print(int(x)/int(y))
+    if c=="+":
+    
+        print(int(x)+int(y))
+
+
+
+
+
+
+        
+
+        
+        
