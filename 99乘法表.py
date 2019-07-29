ae59780479ad9abb67bd1980206af4c0da60d43a
veryspecialdog <13141360658@163.com>
2019-07-30 00:47:12 +0800

    Update 99乘法表.py

--- 99乘法表.py
+++ 99乘法表.py
@@ -1,6 +1,8 @@
 for x in range(1,10):
     for y in range(x,10):
-            print("{}X{}={}\t".format(x,y,x*y),end='')
+            print("{}X{}={}\t".format(y,x,x*y),end='')
+    print()
+                
         
     
 
