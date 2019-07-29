78e5a6ccc9c611b41e8fba8189d882c967d79c7e
veryspecialdog <13141360658@163.com>
2019-07-30 01:00:49 +0800

    Update 99乘法表.py

--- 99乘法表.py
+++ 99乘法表.py
@@ -1,7 +1,15 @@
 for x in range(1,10):
     for y in range(1,x+1):
-            print("{}X{}={}\t".format(y,x,x*y),end='')
-    print()
+            print("{}X{}={}\t".format(y,x,x*y))
+    print()                 
+            
+                    
+                           
+
+
+                    
+                   
+            
                 
         
     
