e223700d477b7ac642face670c91bd0032a22743
veryspecialdog <13141360658@163.com>
2019-08-03 00:25:19 +0800

    Update 99乘法表.py

--- 99乘法表.py
+++ 99乘法表.py
@@ -2,8 +2,18 @@ x = 1
 while x < 10:
     y = 1
     while y <= x:
-        print(x,y)
-        break
+        print()
+        while y <= x:
+            print('%d*%d=%d\t' %(y, x, x*y) , end='')
+        y +=1
+    
+    print()
+    x +=1
+print()
+
+
+
+        
         
     
     
