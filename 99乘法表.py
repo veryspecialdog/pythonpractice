6d2a169e10e4872a95b35255375c3c2915af530e
veryspecialdog <13141360658@163.com>
2019-08-02 23:07:54 +0800

    Update 99乘法表.py

--- 99乘法表.py
+++ 99乘法表.py
@@ -1,14 +1,27 @@
 x=0
-while x<9:
-    
+while x <9:
     x +=1
-    y =0
+    y=0
     while y<9:
-        y +=1
-        if y % 2 !=0:
-            print("{}x{}={}\t".format(x,y,x*y),end='')
+        y=x+1
+        print(x,y)
+    pirnt()    
+
+    
+     
+
+
+    
         
-    print()
+    
+    
+            
+            
+        
+        
+            
+        
+    
         
             
             
