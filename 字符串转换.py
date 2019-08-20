fe2411a82a1659dd1947e03d303a2813afefe2a5
veryspecialdog <13141360658@163.com>
2019-08-20 23:42:35 +0800

    Update 字符串转换.py

--- 字符串转换.py
+++ 字符串转换.py
@@ -1,5 +1,20 @@
 v=[1,2,3,4,5,6,7,8,9]
-print(v[3:6])
+v.reverse()
+v1=str(v)[2:7]
+v2=v1[::-1]
+v3=v2[2:3]
+v4=int(v3)
+v5="{num:b}".format(num=v4)
+v6="{num:d}".format(num=v4)
+v7="{num:o}".format(num=v4)
+print(v5,v6,v7)
+
+
+
+
+
+
+
 
 
 
