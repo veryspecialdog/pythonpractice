1cc0a53a9bdcf7527a69a4cb9f48d826c17b812b
veryspecialdog <13141360658@163.com>
2019-09-06 00:41:29 +0800

    Update 创建函数.py

--- 创建函数.py
+++ 创建函数.py
@@ -1,25 +1,4 @@
-import math as foobar
-print(foobar.sqrt(4))
-
-x,y,z = 1,2,3
-print(x,y,z)
-
-x,y=y,x
-print(x,y,z)
-
-values =1,2,3
-print(values)
-
-x,y,z = values
-print(x)
-
-name = input('what i your name?')
-if name.endswith('Gumby'):
-    if name.startwith('Mr.'):
-        print('hello,mr. gumnby')
-    elif name.startswith('Mrs.'):
-        print('hello,mrs.gumby')
-    else:
-        print('hello,gumby')
-else:
-    print('hello,stranger')
+x = 1
+while x<=10:
+    print(x)
+    x +=1
