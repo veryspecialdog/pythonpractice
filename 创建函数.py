5caf116f23213506ef05efc2affada0eb813a615
veryspecialdog <13141360658@163.com>
2019-09-06 01:04:16 +0800

    Update 创建函数.py

--- 创建函数.py
+++ 创建函数.py
@@ -1,10 +1,15 @@
 x = 1
 while x<=10:
-    y = 1
-    while y<10:
-        print(x,y)
+    y =1
+    while y<=10:
         x +=1
-        y +=1
+        print(x)
+        break
+
+
+   
+        
+        
         
     
     
