dbc6533d8e3b9f468c80c57999679568f5d8bfa7
veryspecialdog <13141360658@163.com>
2019-09-09 12:40:10 +0800

    Update 创建函数.py

--- 创建函数.py
+++ 创建函数.py
@@ -1,11 +1,33 @@
 x=1
-while x<=10:
-    
-    x +=1
+while x<=9:
     y=1
-    while y<=10:
+    while y<=x:
+        print("{}*{}={}\t".format(x,y,x*y),end='')
         y +=1
-        print(y,x)
+    print()
+    x +=1
+print()
+
+
+
+names = ['anne','beth','george','damon']
+ages = [12,45,32,102]
+
+for i in range(len(names)):
+    print(names[i],'is',ages[i],'years old')
+
+print(list(zip(names,ages)))  
+
+from math import sqrt
+for n in range(99,0,-1):
+    root = sqrt(n)
+    if root == int(root):
+        print(n)
+        break
+
+
+
+
         
         
         
