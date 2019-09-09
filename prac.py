930966ea0b95e7dcac824f929e6dc238ca183401
veryspecialdog <13141360658@163.com>
2019-09-09 23:10:39 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,5 +1,8 @@
-list1 = ['key1','key2','key3']
-list2 = ['1','2','3']
-d=dict(zip(list1,list2))
-print(d)
+words = ['this','is','an','ex','parrot']
+for words in words:
+    print(words)
 
+
+numbers =[0,1,2,3,4,5,6,7,8,9]
+for number in numbers:
+    print(number)
