220ff4d39f080641baa3b40fe142701a14bb3a99
veryspecialdog <13141360658@163.com>
2019-09-18 22:26:37 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,6 +1,8 @@
-s = 'Python'
-L =list(s)
-print(s)
-print(L)
-del L[2]
-print(L)
+import random
+n = 10
+a_list =[random.randrange(1,100) for i in range(n)]
+print(f'a_list comprehends {len(a_list)} random number:\n',a_list)
+a_list.sort()
+print('the list sorted:\n',a_list)
+a_list.sort(reverse=True)
+print('the list sorted reversely:\n',a_list)
