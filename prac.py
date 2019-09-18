e7bcbec89f20bf7443e2c19ff61bc18e02fd6403
veryspecialdog <13141360658@163.com>
2019-09-18 08:00:31 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,4 +1,7 @@
-a_list = []
-a_list.append(1)
-a_list.append(2)
-print(a_list,f'has a length of {len(a_list)}.')
+a_list = [1,2,3]
+b_list = [4,5,6]
+c_list = a_list + b_list*3
+print(c_list)
+
+print(7 not in c_list)
+print(a_list > b_list)
