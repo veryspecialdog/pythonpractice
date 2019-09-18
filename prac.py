361beaeb3dd658aced9c4322a42c9b8e3ad3bf25
veryspecialdog <13141360658@163.com>
2019-09-18 08:14:04 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,7 +1,6 @@
-a_list = [1,2,3]
-b_list = [4,5,6]
-c_list = a_list + b_list*3
-print(c_list)
-
-print(7 not in c_list)
-print(a_list > b_list)
+s = 'Python'
+L =list(s)
+print(s)
+print(L)
+del L[2]
+print(L)
