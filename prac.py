61e4717a1816a966bd84999a919fd45b8aceacad
veryspecialdog <13141360658@163.com>
2019-09-21 21:21:05 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -4,7 +4,7 @@ from matplotlib_venn import venn2
 admins = {'Moose','Joker','Joker'}
 moderators = {'Ann','Chris','Jane','Moose','Zero'}
 
-v=venn2(subsets=(admins,moderators),set_labels=('admins','moderators'))
+v = venn2(subsets=(admins,moderators),set_babels =('admins','moderators'))
 v.get_label_by_id('11').set_text('\n'.join(admins & moderators))
 v.get_label_by_id('10').set_text('\n'.join(admins - moderators))
 v.get_label_by_id('01').set_text('\n'.join(admins ^ moderators))
