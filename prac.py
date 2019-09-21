853d69e8ae904d6adfc0b88e2f9f670c7ca209d5
veryspecialdog <13141360658@163.com>
2019-09-21 23:01:12 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,3 +1,3 @@
-phonebook = {'ann':6575,'bob':8982,'joe':2598,'zoe':1225}
-print(phonebook)
-print(phonebook['bob'])
+phonebook1 ={'ann':6575,'bob':8982,'joe':2598,'zoe':1225,'ann':6585}
+print(phonebook1['joe'])
+print(phonebook1['joe'] = 5802)
