fdf6a5ef0b166c46b11edb7ed3d76c3dcec18d49
veryspecialdog <13141360658@163.com>
2019-09-21 22:54:35 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,12 +1,3 @@
-import matplotlib.pyplot as plt 
-from matplotlib_venn import venn2
-
-admins = {'Moose','Joker','Joker'}
-moderators = {'Ann','Chris','Jane','Moose','Zero'}
-
-v = venn2(subsets=(admins,moderators),set_babels =('admins','moderators'))
-v.get_label_by_id('11').set_text('\n'.join(admins & moderators))
-v.get_label_by_id('10').set_text('\n'.join(admins - moderators))
-v.get_label_by_id('01').set_text('\n'.join(admins ^ moderators))
-
-plt.show()
+phonebook = {'ann':6575,'bob':8982,'joe':2598,'zoe':1225}
+print(phonebook)
+print(phonebook['bob'])
