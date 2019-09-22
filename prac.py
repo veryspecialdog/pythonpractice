b7e59fa8229518199d90ae14c4df12e55fad1e01
veryspecialdog <13141360658@163.com>
2019-09-22 14:06:27 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,3 +1,3 @@
-phonebook1 ={'ann':6575,'bob':8982,'joe':2598,'zoe':1225,'ann':6585}
-print(phonebook1['joe'])
-print(phonebook1['joe'] = 5802)
+s = 'Python'
+for i,c in enumerate(s):
+    print(i,c)
