1b91c060a0dd4f07da3aad239543291a2b255d1e
veryspecialdog <13141360658@163.com>
2019-10-18 22:10:00 +0800

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,10 +1,4 @@
-def is_leap(year):
-    leap = False
-    if year % 4 == 0:
-        leap = True
-        if year % 100 ==0 and year % 400 !=0:
-            leap = False
-    return leap
+def do_sonmething():
+    print('this is a hello message from do_something().')
 
-print(is_leap(7))
-
+print(do_sonmething())
