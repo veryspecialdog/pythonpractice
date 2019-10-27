3a18e4305956c7d7533bc16af802278698995ce8
veryspecialdog <13141360658@163.com>
2019-10-27 15:42:05 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,6 +1,22 @@
-try:
-    f = open('test_file.text','r')
-except FileNotFoundError as fnf_error:
-    print(fnf_error)
-   
-       
+def routine_1():
+    print('Routine 1 done.')
+
+def routine_2():
+    sub_routine_1()
+    sub_Routine_2()
+    print('Routine 2 done.')
+
+
+def sub_routine_1():
+    print('Sub-routine 1 done.')
+
+def sub_Routine_2():
+    print('Sub-routine 2 done.')
+
+def main():
+    routine_1()
+    routine_2()
+    print('this is the end of program')
+
+if _name_=='_main_':
+    main()
