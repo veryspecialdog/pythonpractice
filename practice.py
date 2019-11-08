c6fc5af7e81f0e06078abb47422e806f4b639b7b
veryspecialdog <13141360658@163.com>
2019-11-08 22:48:06 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,18 +1,9 @@
 class Golem:
     def __init__(self,name=None):
         self.name = name
-        self.built_year = datetime.data.today().year 
+        self.built_year = datatime.data.today().year 
+    
     def say_hi(self):
         print('Hi!')
-    
-    g = Golem('Clay')
-    g.name
-    g.built_year
-    g.say_hi
-    g.say_hi()
-    type(g)
-    type(g.name)
-    type(g.built_year)
-    type(g.__init__)
-    type(g.say_hi)
+print(g=Golem('Clay'))
 
