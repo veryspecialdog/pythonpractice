ca7eea510473ab197da4cd52ec52981488ffc0c4
veryspecialdog <13141360658@163.com>
2019-11-08 22:50:57 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,7 +1,9 @@
+import dateime
+
 class Golem:
     def __init__(self,name=None):
         self.name = name
-        self.built_year = datatime.data.today().year 
+        self.built_year = datetime.data.today().year 
     
     def say_hi(self):
         print('Hi!')
