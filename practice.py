553952a4e1bfccb0021a9dba6281c3446c299143
veryspecialdog <13141360658@163.com>
2019-11-08 22:24:27 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,22 +1,6 @@
-def routine_1():
-    print('Routine 1 done.')
+for i in range(65,97):
+    print(i)
 
-def routine_2():
-    sub_routine_1()
-    sub_Routine_2()
-    print('Routine 2 done.')
-
-
-def sub_routine_1():
-    print('Sub-routine 1 done.')
-
-def sub_Routine_2():
-    print('Sub-routine 2 done.')
-
-def main():
-    routine_1()
-    routine_2()
-    print('this is the end of program')
-
-if _name_=='_main_':
-    main()
+help(chr)
+    
+    
