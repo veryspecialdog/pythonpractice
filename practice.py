14460a89f1b1b86b58e45e35559041fe07728910
veryspecialdog <13141360658@163.com>
2019-11-09 23:57:29 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -9,4 +9,14 @@ class Golem:
         print('Hi!')
 
 g = Golem('Clay')
+print(g.name)
+print(g.built_year)
+print(g.say_hi)
+print(g.say_hi())
+type(g)
+type(g.name)
+type(g.built_year)
+type(g.__init__)
+type(g.say_hi)
+
 
