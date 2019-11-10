6298642d158cb744584d8cc14602a7f3aedb50cc
veryspecialdog <13141360658@163.com>
2019-11-10 16:13:36 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,16 +1,24 @@
 import datetime
 
 class Golem:
-    def _init_(self,name=None):
-        self.name = name
-        self.built_year = datetime.date.today().year
-    
+    def __init__(self,name = None):
+    self.name = name 
+    self.built_year = datetime.date.today().year
+
     def say_hi(self):
         print('Hi!')
 
-g = Golem('Clay')
+class runningGolem(Golem):
+    def run(self):
+        print("can't you see? I'm running...")
+    
+    def say_hi(self):
+        print('Hey! Nice day,Huh?')
 
-print(g.name)
+rg = runningGolem('Clay')
+
+    
+
 
 
 
