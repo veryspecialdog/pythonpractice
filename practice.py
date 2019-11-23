4435cfe97fa7db5f2ee51c5d43f8f32942a1d383
veryspecialdog <13141360658@163.com>
2019-11-24 00:20:39 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,18 +1,12 @@
-def is_prime(n):
-    if n <2:
-        return False
+for n in range(2,100):
     if n == 2:
-        return True
-    for m in range(2,int(n**0.5)+1):
-        if (n % m) == 0:
-            return False
-        else:
-            return True
-
-for i in range(80,110):
-    if is_prime(i):
-        print(i)
-
+        print(n)
+        continue
+    for i in range(2,n):
+        if (n%i) == 0:
+            break
+    else:
+        print(n)
     
 
 
