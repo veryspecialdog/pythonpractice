8c64a3ba4701fef93fb939d4e54390b1fc022dc0
veryspecialdog <13141360658@163.com>
2019-12-01 23:24:41 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,12 +1,4 @@
-for n in range(2,100):
-    if n == 2:
-        print(n)
-        continue
-    for i in range(2,n):
-        if (n%i) == 0:
-            break
-    else:
-        print(n)
+print(ord('c'))
     
 
 
