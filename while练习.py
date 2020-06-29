307c0dcd04a86667952ecba67c554976bfc0383e
veryspecialdog <13141360658@163.com>
2020-06-29 13:32:35 +0800

    Update while练习.py

--- while练习.py
+++ while练习.py
@@ -1,38 +1,15 @@
-import random
-n = 3
-a_list = [random.randrange(65,91) for i in range(n)]
-b_list = [chr(random.randrange(65,91)) for i in range(n)]
-print(a_list)
-c_list = a_list + b_list + a_list*2
-print(c_list)
-
-print(c_list[3])
-print(c_list[:])
-print(c_list[5:])
-print(c_list[:3])
-print(c_list[2:6])
-
-del c_list[3]
-print(c_list)
-
-c_list[1:5:2] = ['a',2]
-
-print(c_list)
+for i in range(1,31):
+        print(i)
+        
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+       
+        
+        
+      
+        
 
 
 
