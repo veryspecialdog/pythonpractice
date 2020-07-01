2e0c1ef902271a57d1011c2986292312f4b9d3dd
veryspecialdog <13141360658@163.com>
2020-07-01 22:29:49 +0800

    11

---
+++ pra.py
@@ -0,0 +1,2 @@
+phonebook ={'ann':6575,'bob':8982,'job':2598,'zoe':1225}
+print(phonebook)
--- prac.py
+++ prac.py
@@ -1,91 +1,14 @@
-import random
-n = 10
-a_list =[random.randrange(1,100) for i in range(n)]
-print(f'a_list comprehends {len(a_list)} random numbers:\n',a_list)
+#制作这个cell集合运算图示，需要转转matplotlib和matpltlib-venn
+import matplotlib.pyplot as plt
+from matplotlib_venn import venn2
 
-a_list.sort()
-print('the list sorted:\n',a_list)
+admins ={'Moose','Joker','Joker'}
+moderators ={'Ann','Chris','Jane','Moose','Zero'}
 
-a_list.sort(reverse=True) #reverse参数，默认值是False
-print('the list sorted reversely:\n',a_list)
+v = venn2(subsets =(admins,moderators),set_labels=('admins','moderators'))
+v.get_label_by_id('11').set_text('\n'.join(admins&moderators))
+v.get_label_by_id('10').set_text('\n'.join(admins-moderators))
+v.get_label_by_id('01').set_text('\n'.join(admins ^ moderators))
 
-import random
-n = 10
-a_list = [chr(random.randrange(65,91)) for i in range(n)]
-#chr()函数会返回指定ASCII码的字符，ord('A')是65
-print(f'a_list comprehends {len(a_list)} random string elements:\n',a_list)
-
-a_list.sort()
-print('the list sorted:\n',a_list)
-
-a_list.sort(reverse=True) #reverse参数，默认值是False
-print('the list sorted reversely:\n',a_list)
-
-b_list = [chr(random.randrange(65,91))+chr(random.randrange(97,123)) for i in range(n)]
-
-print(f'b_list comprehends {len(b_list)} random string elements:\n',b_list)
-
-b_list.sort()
-print('sorted:\n',b_list)
-
-b_list.sort(key=str.lower,reverse=True)
-#key参数，默认值是None
-#key=str.lower的意思是：先将字母全都转换成小写，在进行比较排序
-#但是，这样做不会改变原值
-print('the sorted reversely:\n',b_list)
-
-import random
-n=3
-a_list = [random.randrange(65,91) for i in range(n)]
-b_list = [chr(random.randrange(65,91)) for i in range(n)]
-print(a_list)
-c_list = a_list + b_list +a_list*2
-
-print(c_list)
-
-c_list.append('100')
-print(c_list)
-print()
-print(a_list)
-a_list.clear()
-print(a_list)
-
-print()
-print(a_list)
-a_list.clear()
-print(a_list)
-
-#复制一个列表
-d_list = c_list.copy()
-print(d_list)
-del d_list[6:8]
-print(d_list)
-print(c_list)
-
-e_list = d_list
-del e_list[6:8]
-print(e_list)
-print(d_list)
-
-print(a_list)
-a_list.extend(c_list)
-print(c_list)
-
-#在索引的某个位置插入一个元素
-print(a_list)
-a_list.insert(1,'example')
-a_list.insert(3,'example')
-print(a_list)
-
-#排序
-
-#a_list.sort()这一句出错，因为当前列表中的元素是由int和str混合组成的
-
-print()
-print(a_list)
-a_list.reverse()
-print(a_list)
-x = a_list.reverse()
-print(x)
-
+print(plt.show())
 
