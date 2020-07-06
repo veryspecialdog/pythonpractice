4b0c89c777c4af2b12e507fe75394e24c26ebbd5
veryspecialdog <13141360658@163.com>
2020-07-06 19:08:59 +0800

    Update find100englishworlds.py

--- find100englishworlds.py
+++ find100englishworlds.py
@@ -2,6 +2,5 @@ import requests
 from lxml import etree
 
 url ='https://maoyan.com/films?catId=3&sourceId=2&yearId=15&showType=3'
-header = {"User Agent":'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
+header = {"User Agent":'Mozilla/5.0 (Windows NT 10.0;Win64;x64;rv:78.0) Gecko/20100101 Firefox/78.0'}
 html = requests.get(url,headers=header).content.decode('utf-8')
-print(html)
