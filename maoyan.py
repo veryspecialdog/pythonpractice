ad1c9b9aee4d0ef5a9e182222671439c3b934651
veryspecialdog <13141360658@163.com>
2020-07-08 16:14:43 +0800

    111

--- maoyan.py
+++ maoyan.py
@@ -1,6 +1,6 @@
 import requests
 from lxml import etree
 
-url ='https://movie.douban.com/top250'
+url ='https://maoyan.com/films?catId=3&showType=3'
 html = requests.get(url).content.decode('utf-8')
 print(html)
