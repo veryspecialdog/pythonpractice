1c0a0bcd660195eedd8357c78b29c16f5099dae1
veryspecialdog <13141360658@163.com>
2020-07-08 15:17:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 from lxml import etree
 
-url ='https://movie.douban.com/chart'
+url ='http://www.mtime.com/top/movie/top100/'
 html = requests.get(url).content.decode('utf-8')
 print(html)
