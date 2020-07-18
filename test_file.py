69987c8a178ee8b8cc00842dbe0e174ee0ff4b31
veryspecialdog <13141360658@163.com>
2020-07-18 19:35:05 +0800

    111

--- test_file.py
+++ test_file.py
@@ -3,10 +3,14 @@ def sum_of_word(word):
     for char in word:
         sum += ord(char) - 96
     return sum
+with open('results.txt','w') as result:
+    with open('words_alpha.txt','r') as file:
+        for word in file.readlines():
+            if sum_of_word(word.strip()) == 100:
+                result.write(word)
+          
+    
 
-with open('words_alpha.txt','r') as file:
-    for word in file.readlines():
-        if sum_of_word(word.strip()) == 100:
-            print(word)
-            
+
+                       
 
