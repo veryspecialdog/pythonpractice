9f6b08503fe9a9afed7dcd4353f4cf8d54d3bc57
veryspecialdog <13141360658@163.com>
2020-07-18 18:39:44 +0800

    11

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import requests
 from lxml import etree
 
-url ='https://maoyan.com/board/4'
-html = requests.get(url).content.decode('utf-8')
+url='https://music.douban.com'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+html=requests.get(url,headers =headers)
 print(html)
 
     
