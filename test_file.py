b5bf55e1d83aeb8b76c728171a5ef32660594cd2
veryspecialdog <13141360658@163.com>
2020-07-18 18:45:30 +0800

    11

--- test_file.py
+++ test_file.py
@@ -1,3 +1,10 @@
-first line
- second line 
- third line
+import os
+
+with open('test-file.txt','w') as f:
+    f.write('first line\n second line\n third line\n')
+
+with open('test-file.txt','r') as f:
+    for line in f.readlines():
+        print(line)
+        
+
