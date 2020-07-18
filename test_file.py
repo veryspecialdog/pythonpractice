f31eb491b3946b1b6c5c9d6fdb79f4b6f950b2ca
veryspecialdog <13141360658@163.com>
2020-07-18 18:53:32 +0800

    11

--- test_file.py
+++ test_file.py
@@ -1,10 +1,4 @@
-import os
-
-with open('test-file.txt','w') as f:
-    f.write('first line\n second line\n third line\n')
-
-with open('test-file.txt','r') as f:
-    for line in f.readlines():
-        print(line)
-        
+with open('words_alpha.txt','r') as file:
+    for word in file.readlines():
+        break   
 
