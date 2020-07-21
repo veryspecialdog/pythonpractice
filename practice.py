99a69a1959944c143133a3b50629eb435565683f
veryspecialdog <13141360658@163.com>
2020-07-21 20:51:45 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,7 +1,8 @@
-cd ~ /Downloads
-git clone https://github.com/python/cpython.git
-cd cpython/Doc/tutorial
-ls
+def do_something():
+    print('this id a hello message from do_something().')
+if not do_something():
+    print("the return value of'do_something()' is None.")
+
 
 
 
