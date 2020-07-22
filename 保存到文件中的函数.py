ac5b51fa14e70a75ccb545699ca8304a68f4854c
veryspecialdog <13141360658@163.com>
2020-07-23 01:42:08 +0800

    Update 保存到文件中的函数.py

--- 保存到文件中的函数.py
+++ 保存到文件中的函数.py
@@ -1,4 +1,12 @@
-import mycode
+import this
+s=this.s
+print(s)
 
+d ={}
+for c in (65,97):
+    for i in range(26):
+        d[chr(i+c)] = chr((i+13) % 26 +c)
+    
+print("".join([d.get(c,c) for c in s]))
 
-print(mycode.is_prime(3))
+print(this.d)
