1811da020b304b57197111eead9c348c7430b991
veryspecialdog <13141360658@163.com>
2020-07-22 19:07:57 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,7 +1,18 @@
-def be_careful(a,b):
-    a =2
-    b[0] ='What?!'
+def say_hi(*names):
+    for name in names:
+        print(f'Hi,{name}!')
 
-a =1
-b =[1,2,3]
-print(be_careful(a,b))
+names = ('mike','John','zeo')
+say_hi(*names)
+
+a_string ='Python'
+say_hi(*a_string)
+
+a_range =range(10)
+say_hi(*a_range)
+
+a_list = list(range(10,0,-1))
+say_hi(*a_list)
+
+a_dictionary ={'ann':2321,'mike':8712,'joe':7610}
+say_hi(*a_dictionary)
