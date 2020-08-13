3c25ecd27a5443dc76f8869b269d8fe9e318a783
veryspecialdog <13141360658@163.com>
2020-08-13 19:33:02 +0800

    1

--- practice.py
+++ practice.py
@@ -1,3 +1,10 @@
-import sys
-a=str(sys.builtin_module_names)
-print(a)
+def is_leap(year):
+    if year %4  == 0:
+        print(f'{} is leap year!')
+    else:
+        print(f'{} is not leap year!')
+
+    
+
+print(is_leap(4))
+
