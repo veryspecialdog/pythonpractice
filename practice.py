d451c14916bcd8a8254bf058a00eff466201c2b1
veryspecialdog <13141360658@163.com>
2020-08-15 19:52:04 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,13 +1,12 @@
-import this
-love = this
+import datetime
 
-print(id(this))
-print(id(love))
+class Golem:
 
-
-        
-
-        
-        
+    def __init__(self,name=None):
+        self.name = name
+        self.built_year = datetime.date.today().year
     
-        
+    def say_hi(self):
+        print('Hi!')
+g = Golem('Clay')
+print(g.name)
