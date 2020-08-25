a0e90d02c963b8f9270f0f0a138c86d5b96b3626
veryspecialdog <13141360658@163.com>
2020-08-25 16:26:40 +0800

    Update 正则表达式practice.py

--- 正则表达式practice.py
+++ 正则表达式practice.py
@@ -1,2 +1,7 @@
-import string
-print(string.ascii_letters)
+import re
+str = 'The white dog wears a black hat.'
+pttn = r'The (white|black) dog wears a (white|black) hat.'
+re.findall(pttn,str)
+
+repl = r'The \2 dog wears a \1 hat.'
+print(re.sub(pttn,repl,str))
